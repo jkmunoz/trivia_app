a66afef13150b87aceb8744ca4a2ed88d32f68e2
jkmunoz <juliannakmunoz@gmail.com>
2023-07-16 20:34:37 -0500

    set up CORS/after-request decorator

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,14 +12,23 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    CORS(app)  
 
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
-
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Origins", '*')
+        response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,OPTIONS,DELETE")
+        return response
+
+
+    
 
     """
     @TODO:
